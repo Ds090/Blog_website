e2399e14e5f7c4907da86b0130ce37bcef67350d
Divanshu Kachhawa <divanshukachawa7878@gmail.com>
2024-04-25 01:36:15 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Add react-router-dom to the list of external dependencies
-const externalDependencies = ['react-router-dom'];
-
 export default defineConfig({
   plugins: [
-    // Use the react plugin
     react()
   ],
-  // Add build configuration to treat "react-router-dom" as an external dependency
-  build: {
-    rollupOptions: {
-      external: externalDependencies
-    }
+  optimizeDeps: {
+    exclude: ['react-router-dom']
   }
 });
-
-

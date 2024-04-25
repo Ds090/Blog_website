9a54058dc0dcdb62f9e60cd8ccf1c251190a3afa
Divanshu Kachhawa <divanshukachawa7878@gmail.com>
2024-04-26 00:32:58 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    // Explicitly specify the root directory
+    root: process.cwd()
+  }
 });
+
+
 
 

ff0f88aa1a262a3a572098f6dcc8d792358146fb
Divanshu Kachhawa <divanshukachawa7878@gmail.com>
2024-04-26 10:38:05 +0530

    first commit

--- src/assets/Add_Blog/AddBlog.jsx
+++ src/assets/Add_Blog/AddBlog.jsx
@@ -26,7 +26,7 @@ function AddBlog({ handleAddBlog, handleClose }) {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-0 flex justify-center items-center addAnimation">
-      <form className="w-[450px] h-[500px] flex flex-col justify-center items-center p-1 bg-[#F4F6FE] gap-2 rounded-xl m-8">
+      <form className="lg:w-[450px] lg:h-[500px] flex flex-col justify-center items-center p-1 bg-[#F4F6FE] gap-2 rounded-xl m-8 w-[300px] ">
         <div className="w-full flex justify-end h-[20px] mr-6">
           <button onClick={handleClose}>
             <GrClose className="text-[20px] font-bold" />
@@ -35,7 +35,7 @@ function AddBlog({ handleAddBlog, handleClose }) {
         <input
           type="text"
           placeholder="Enter Blog Post URL"
-          className="border w-[400px] py-1 px-1 font-semibold text-[16px]"
+          className="border lg:w-[400px] py-1 px-1 font-semibold text-[16px]"
           value={post}
           onChange={(e) => {
             setPost(e.target.value);
@@ -44,7 +44,7 @@ function AddBlog({ handleAddBlog, handleClose }) {
         <input
           type="text"
           placeholder="Enter Blog Title"
-          className="border w-[400px] py-1 px-1 font-semibold text-[16px]"
+          className="border lg:w-[400px] py-1 px-1 font-semibold text-[16px]"
           value={title}
           onChange={(e) => {
             setTitle(e.target.value);
@@ -53,7 +53,7 @@ function AddBlog({ handleAddBlog, handleClose }) {
         <input
           type="text"
           placeholder="Enter Blog Description"
-          className="border w-[400px] py-1 px-1 font-semibold text-[16px]"
+          className="border lg:w-[400px] py-1 px-1 font-semibold text-[16px]"
           value={description}
           onChange={(e) => {
             setDescription(e.target.value);
@@ -63,14 +63,14 @@ function AddBlog({ handleAddBlog, handleClose }) {
           cols="30"
           rows="10"
           placeholder="Write..."
-          className="border w-[400px] py-1 px-1 font-semibold text-[16px]"
+          className="border lg:w-[400px] py-1 px-1 font-semibold text-[16px]"
           value={blogContent}
           onChange={(e) => {
             setBlogContent(e.target.value);
           }}
         ></textarea>
         <button
-          className="bg-[#6F11F5] w-[400px] py-1 rounded-md text-white font-semibold hover:bg-purple-700 hover:transition hover:duration-500 hover:ease-in-out hover:delay-150"
+          className="bg-[#6F11F5] lg:w-[400px] w-full py-1 rounded-md text-white font-semibold hover:bg-purple-700 hover:transition hover:duration-500 hover:ease-in-out hover:delay-150"
           onClick={setBlogPost}
         >
           Add Blog
